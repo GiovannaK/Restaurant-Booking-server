de2b7bd6ed59150be087bf8dad8c99d968cff05c
Giovanna Cunha <giovannakelli@gmail.com>
2021-04-12 22:30:12 -0300

    fix: home pagination was temporarily removed

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,13 +3,7 @@ const Restaurant = require('../models/Restaurant');
 
 exports.index = async (req, res) => {
   try {
-    const page = Number(req.query.page) || 1;
-    const pageSize = Number(req.query.limit) || 10;
-    const skip = (page - 1) * pageSize;
-    const total = await Restaurant.countDocuments();
-    const pages = Math.ceil(total / pageSize);
-
-    const restaurants = await Restaurant.find().skip(skip).limit(pageSize).populate('images')
+    const restaurants = await Restaurant.find().populate('images')
       .populate('restaurantCategory');
 
     if (!restaurants) {
@@ -20,20 +14,9 @@ exports.index = async (req, res) => {
       });
     }
 
-    if (page > pages) {
-      return res.status(400).json({
-        success: false,
-        message: 'Page does not exists',
-        status: 400,
-      });
-    }
-
     return res.status(200).json({
       success: true,
       restaurants,
-      page,
-      pages,
-      total,
       status: 200,
     });
   } catch (error) {
